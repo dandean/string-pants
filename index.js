250ffae0cd0065fccdbf086654d2f6580c5a0f36
Dan Dean <dan.dean@simple.com>
2012-08-11 15:50:11 -0700

    Move all module methods to named functions.

--- index.js
+++ index.js
@@ -1,68 +1,73 @@
 ;(function() {
   'use strict';
 
+
+  /**
+   * String#camel() -> String
+   *
+   * Converts the string to camel case.
+  **/
+  function camel() {
+    // https://github.com/sstephenson/prototype/blob/1fb9728/src/lang/string.js#L560
+    return this.getWords().join(' ').replace(/\s+(.)?/g, function(match, chr) {
+      return chr ? chr.toUpperCase() : '';
+    });
+  }
+
+  /**
+   * String#dash() -> String
+   *
+   * Converts the string to dash-format.
+  **/
+  function dash() {
+    return this.getWords().join('-');
+  }
+
+  /**
+   * String#underscore() -> String
+   *
+   * Converts the string to underscore format.
+  **/
+  function underscore() {
+    return this.getWords().join('_');
+  }
+
+  /**
+   * String#getWords() -> Arrat
+   *
+   * Extracts all individual words from a string and returns them as an Array.
+  **/
+  function getWords() {
+    // Some regular expressions from here:
+    // https://github.com/sstephenson/prototype/blob/1fb9728/src/lang/string.js#L602
+    return this
+      // recognize XXx
+      .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2')
+      // recognize x8xX
+      .replace(/([a-z\d])([A-Z])/g, '$1 $2')
+      .toLowerCase()
+      // Replace anything that's not a letter/number with a single space
+      .replace(/[^a-z0-9]+/g, ' ')
+      .trim()
+      .split(' ');
+  }
+
+  /**
+   * String#capital() -> String
+   *
+   * Capitalizes the first word of the string and lowercases the rest.
+  **/
+  function capital() {
+    // https://github.com/sstephenson/prototype/blob/1fb9728/src/lang/string.js#L580
+    return this.charAt(0).toUpperCase() + this.substring(1).toLowerCase();
+  }
+
   var Extensions = {
-
-    /**
-     * String#camel() -> String
-     *
-     * Converts the string to camel case.
-    **/
-    camel: function() {
-      // https://github.com/sstephenson/prototype/blob/1fb9728/src/lang/string.js#L560
-      return this.getWords().join(' ').replace(/\s+(.)?/g, function(match, chr) {
-        return chr ? chr.toUpperCase() : '';
-      });
-    },
-
-    /**
-     * String#dash() -> String
-     *
-     * Converts the string to dash-format.
-    **/
-    dash: function() {
-      return this.getWords().join('-');
-    },
-
-    /**
-     * String#underscore() -> String
-     *
-     * Converts the string to underscore format.
-    **/
-    underscore: function() {
-      return this.getWords().join('_');
-    },
-
-    /**
-     * String#getWords() -> Arrat
-     *
-     * Extracts all individual words from a string and returns them as an Array.
-    **/
-    getWords: function() {
-      // Some regular expressions from here:
-      // https://github.com/sstephenson/prototype/blob/1fb9728/src/lang/string.js#L602
-      return this
-        // recognize XXx
-        .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2')
-        // recognize x8xX
-        .replace(/([a-z\d])([A-Z])/g, '$1 $2')
-        .toLowerCase()
-        // Replace anything that's not a letter/number with a single space
-        .replace(/[^a-z0-9]+/g, ' ')
-        .trim()
-        .split(' ');
-    },
-
-    /**
-     * String#capital() -> String
-     *
-     * Capitalizes the first word of the string and lowercases the rest.
-    **/
-    capital: function() {
-      // https://github.com/sstephenson/prototype/blob/1fb9728/src/lang/string.js#L580
-      return this.charAt(0).toUpperCase() + this.substring(1).toLowerCase();
-    }
-
+    camel: camel,
+    dash: dash,
+    underscore: underscore,
+    getWords: getWords,
+    capital: capital
   };
 
   var Module = {
